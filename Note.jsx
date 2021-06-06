72588eaf4925360978efa6404bf7256a20098db0
DhruvDwivedi01 <85438996+DhruvDwivedi01@users.noreply.github.com>
2021-06-06 20:50:48 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div  className="note">
+      <h1> Javascript and React.js</h1>
+      <p> This was an amazing bootcamp taken up by Shaurya Sinha
+          We  covered everything from Scratch including Javascript and
+          React.js, HTML. 
+          <h4> THANKYOU </h4> 
+          <h5>DHRUV DWIVEDI </h5></p>
+    </div>
+  );
+}
+
+export default Note;
